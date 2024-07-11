1796c75bbb414fd91d939832ba17220d9941820c
muhammad risano <muhammadrisano@gmail.com>
2024-07-11 13:41:50 +0700

    add update photo profile

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -22,7 +22,35 @@ const profile = async(req, res, next)=>{
         next(new createHttpError.InternalServerError());
     }
 }
+const updateProfile = async(req, res, next)=>{
+    try {
+        const {name, phone, photo} = req.body
+
+        const email = req.decoded.email 
+        const user = await prisma.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+        const userUpdated = await prisma.user.update({
+            data:{
+                name,
+                phone,
+                ...(photo ? {photo}:{})
+            },
+            where:{
+                id: user.id
+            }
+        })
+        response(res, userUpdated, 200, "update profile success");
+
+    } catch (error) {
+        console.log(error);
+        next(new createHttpError.InternalServerError());
+    }
+}
 
 module.exports = {
-    profile
+    profile,
+    updateProfile
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/users')
 const { protect } = require('../middlewares/auth')
 
 router.get('/profile',protect , userController.profile)
+router.put('/profile', protect, userController.updateProfile)
 
 module.exports = router
